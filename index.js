9c845aa6f41efedfaf345708a3e63dc57bcbdbae
sondos alansi <sondos.alansi@yahoo.com>
2018-10-21 20:32:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function theTruth(){
+  return true;
+}
+function sayHey(){
+  
+  return 'Hey!';
+}
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+function favoriteIceCream(ice){
+   return `I love ${ice}`;
+}
+function shouting(str){
+  return str.toUpperCase();
+}
+function roundDown(num){
+  return Math.floor(num);
+}
